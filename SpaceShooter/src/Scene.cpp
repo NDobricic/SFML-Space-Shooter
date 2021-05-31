22e2841dcef53108dd4377c8df381aa97454564f
Nikola Dobricic <nidza.dobricic@gmail.com>
2021-05-31 12:32:18 +0200

    Implemented collision checks. Implemented functions for destroying/spawning objects.

--- SpaceShooter/src/Scene.cpp
+++ SpaceShooter/src/Scene.cpp
@@ -1,10 +1,57 @@
 #include "Scene.h"
+#include <SFML/Graphics.hpp>
+#include <algorithm>
 
 namespace ss
 {
 	void Scene::Update(float deltaTime)
 	{
+		for (int i = 0; i < Particles.size(); i++)
+			Particles[i]->Update(deltaTime);
+
 		for (int i = 0; i < GameObjects.size(); i++)
 			GameObjects[i]->Update(deltaTime);
+
+		for (int i = 0; i < GameObjects.size(); i++)
+			for (int j = i + 1; j < GameObjects.size(); j++)
+			{
+				sf::FloatRect colliderA(GameObjects[i]->Position(), GameObjects[i]->Size());
+				sf::FloatRect colliderB(GameObjects[j]->Position(), GameObjects[j]->Size());
+
+				if (colliderA.intersects(colliderB))
+				{
+					GameObjects[i]->OnCollision(*GameObjects[j]);
+					GameObjects[j]->OnCollision(*GameObjects[i]);
+				}
+			}
+	}
+
+	void Scene::Start()
+	{
+		for (int i = 0; i < Particles.size(); i++)
+			Particles[i]->Start();
+
+		for (int i = 0; i < GameObjects.size(); i++)
+			GameObjects[i]->Start();
+	}
+
+	void Scene::SpawnParticle(Entity* object)
+	{
+		Particles.push_back(object);
+		object->Start();
+	}
+
+	void Scene::SpawnGameObject(Collidable* object)
+	{
+		GameObjects.push_back(object);
+		object->Start();
+	}
+
+	void Scene::DestroyObject(Entity* object)
+	{
+		Particles.erase(std::remove(Particles.begin(), Particles.end(), object), Particles.end());
+		GameObjects.erase(std::remove(GameObjects.begin(), GameObjects.end(), object), GameObjects.end());
+
+		delete object;
 	}
 }
--- SpaceShooter/src/Scene.h
+++ SpaceShooter/src/Scene.h
@@ -2,15 +2,23 @@
 
 #include "Scene.h"
 #include <vector>
-#include "GameObject.h"
+#include "Entity.h"
+#include "Collidable.h"
 
 namespace ss
 {
 	class Scene
 	{
 	public:
-		std::vector<GameObject*> GameObjects;
+		std::vector<Entity*> Particles;
+		std::vector<Collidable*> GameObjects;
 		void Update(float deltaTime);
+		void Start();
+
+		void SpawnParticle(Entity* entity);
+		void SpawnGameObject(Collidable* object);
+		void DestroyObject(Entity* object);
+
 		virtual void Load() = 0;
 		virtual void Unload() = 0;
 	};
